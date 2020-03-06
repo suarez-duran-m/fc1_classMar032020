585a2ba2491528d33359ea4a173f43a234639766
msd <daf@dfa>
2020-03-06 14:33:24 -0500

    Estudiar algun día muy lejano

---
+++ jajaj.py
@@ -0,0 +1,9 @@
+from numpy import loadtxt                                       
+
+data = loadtxt('pitch.txt')                                     
+
+from pylab import plot, show                                    
+
+plot(data)                                                      
+
+show() 
